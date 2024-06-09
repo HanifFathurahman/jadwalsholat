461f82de8b929c867df31ddaeb6c438dcbfd9fa0
Hanif Fathurahman <58112881+HanifFathurahman@users.noreply.github.com>
2024-06-10 06:51:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function updateTime() {
+    const currentTimeElement = document.getElementById('current-time');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    currentTimeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
 function updatePrayerTimes(latitude, longitude) {
     const apiURL = `https://api.aladhan.com/v1/timings?latitude=${latitude}&longitude=${longitude}&method=2`;
     fetch(apiURL)
@@ -47,4 +56,6 @@ function updateEveryMinute() {
 document.addEventListener('DOMContentLoaded', () => {
     fetchLocation();
     updateEveryMinute();
+    updateTime(); // Initial call to display the time immediately when the page loads
+    setInterval(updateTime, 1000); // Update the time every second
 });
